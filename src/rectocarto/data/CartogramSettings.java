5d193d5362cf60db0553b5468f3cb0e10f7832f2
Sander Verdonschot <5864726+Mangara@users.noreply.github.com>
2016-07-07 23:57:13 -0400

    More JavaDoc!

--- src/rectocarto/data/CartogramSettings.java
+++ src/rectocarto/data/CartogramSettings.java
@@ -16,16 +16,64 @@
 package rectocarto.data;
 
 public class CartogramSettings {
+
+    /**
+     * The width (in pixels) of the final cartogram.
+     */
     public double cartogramWidth = 1000;
+    /**
+     * The height (in pixels) of the final cartogram.
+     */
     public double cartogramHeight = 750;
+    /**
+     * The width (in pixels) of the four boundary regions (NORTH, EAST, SOUTH,
+     * WEST).
+     */
     public double boundaryWidth = 30;
+    /**
+     * The minimum width or height (in pixels) of a sea region.
+     */
     public double minimumSeaDimension = 10;
+    /**
+     * The minimum distance (in pixels) between two features of the cartogram.
+     * If two regions are adjacent, they must share at least this much border.
+     * This implicitly defines a minimum width and height for all regions.
+     */
     public double minimumSeparation = 2;
+    /**
+     * The maximum ratio between the height and width (and vice versa) of
+     * non-sea regions.
+     */
     public double maximumAspectRatio = 10;
-    
+
+    /**
+     * The type of objective function that should be used when constructing this
+     * cartogram using (bi)linear programs.
+     */
     public Objective objective = Objective.MAX_AND_AVERAGE_ERROR;
-    
+
     public enum Objective {
-        MAX_ERROR, AVERAGE_ERROR, MAX_AND_AVERAGE_ERROR, AVERAGE_ERROR_SQUARED, MAX_AND_AVERAGE_ERROR_SQUARED
+
+        /**
+         * The maximum cartographic error.
+         */
+        MAX_ERROR, 
+        /**
+         * The average cartographic error.
+         */
+        AVERAGE_ERROR,
+        /**
+         * The average of the maximum and average cartographic error.
+         */
+        MAX_AND_AVERAGE_ERROR,
+        /**
+         * The average of the squares of each region's cartographic error.
+         */
+        AVERAGE_ERROR_SQUARED,
+        /**
+         * The average of the maximum cartographic error and the squares of each
+         * region's cartographic error.
+         */
+        MAX_AND_AVERAGE_ERROR_SQUARED
     }
 }
